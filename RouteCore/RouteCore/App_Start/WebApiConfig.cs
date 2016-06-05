05f7930348ea4a8307de289ac40dd43a8450a8eb
Clyde Shen <clyde.shen@outlook.com>
2016-06-06 10:07:08 +1200

    update reference, enable CORS, add TODO

--- RouteCore/RouteCore/App_Start/WebApiConfig.cs
+++ RouteCore/RouteCore/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Cors;
 
 namespace RouteCore
 {
@@ -10,7 +11,7 @@ namespace RouteCore
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
--- RouteCore/RouteCore/Controllers/InfoController.cs
+++ RouteCore/RouteCore/Controllers/InfoController.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace RouteCore.Controllers
 {
+    //TODO limit CORS header e.g. set origins = some domain, put customer request header, change method
+    [EnableCors("*", "*", "*")]
     public class InfoController : ApiController
     {
+
         [HttpGet]
         public string AboutUs()
         {
